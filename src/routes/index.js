51bad2db5849e5db737fcce3e17b7292951da3fa
Johnny Deymisson <jdeymisson@gmail.com>
2023-03-24 00:30:01 -0300

    aplicando a tratativa de error no server e movendo o middleware para a rota de usuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,18 +5,6 @@ const postsRoutes = require("./posts.routes");
 
 const routes = Router();
 
-// const myMiddlewere = (request, response, next) => {
-//     const { isAdmin } = request.body;
-
-//     if(isAdmin !== true) {
-//         response.status(401).json({ status: "error", message: "Usuário não autorizado!" });
-//         return;
-//     };
-
-//     next();
-// };
-
-
 routes.use("/users", usersRoutes);
 routes.use("/posts", postsRoutes);
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,8 +4,19 @@ const UsersController = require("../controllers/usersController");
 
 const usersRoutes = Router();
 
+const myMiddlewere = (request, response, next) => {
+    const { isAdmin } = request.body;
+
+    if(isAdmin !== true) {
+        response.status(401).json({ status: "error", message: "Usuário não autorizado!" });
+        return;
+    };
+
+    next();
+};
+
 const usersController = new UsersController();
 
-usersRoutes.post("/", usersController.create);
+usersRoutes.post("/", myMiddlewere, usersController.create);
 
 module.exports = usersRoutes;
