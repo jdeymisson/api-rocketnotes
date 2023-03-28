7f7ed5126393a12a059ece31f6d0bb1395f98ac3
Johnny Deymisson <jdeymisson@gmail.com>
2023-03-28 00:46:49 -0300

    finalizando controller de notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -51,11 +51,56 @@ class NotesController {
         note.links = links;
         note.tags = tags;
 
-        response.status(201).json({
+        response.status(200).json({
             ...note,
             tags,
             links
         });
+    };
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex("notes")
+            .where({id})
+            .delete()
+
+        response.status(200).json({ message: "Nota deletada com sucesso."});
+    };
+
+    async index (request, response) {
+        const { user_id, title, tags } = request.query;
+        let notes;
+
+        if(tags) {
+            const filterTags = tags.split(',').map(tag => tag.trim());
+            notes = await knex("tags")
+                .select([
+                    "notes.id",
+                    "notes.title",
+                    "notes.user_id"
+                ])
+                .where("notes.user_id", user_id)
+                .whereLike("notes.title", `%${title}%`)
+                .whereIn("name", filterTags)
+                .innerJoin("notes", "notes.id","tags.note_id")
+                .orderBy("notes.title");
+        } else {
+            notes = await knex("notes")
+            .where({ user_id })
+            .whereLike("title", `%${title}%`)
+        };
+       
+        const userTags = await knex("tags").where({ user_id });
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+            return {
+                ...note,
+                tags: noteTags
+            };
+        });
+
+        response.status(200).json(notesWithTags);
     };
 };
 
