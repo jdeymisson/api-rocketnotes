d8fb1726791b6ac7f18d41a4909c91871c58d0f5
Johnny Deymisson <jdeymisson@gmail.com>
2023-04-13 01:30:46 -0300

    criado a disponibilidade das imagens do avatar

--- src/server.js
+++ src/server.js
@@ -3,11 +3,14 @@ const express = require("express");
 const migrationsRun = require("./database/sqlite/migrations");
 
 const AppError = require("./utils/AppError");
+const uploadConfig = require("./config/upload");
+
 const routes = require("./routes/");
 
 const app = express();
 
 app.use(express.json());
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
